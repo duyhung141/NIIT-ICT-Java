02ae42e432dc882880429ae1ee1612891f864b92
duyhung141 <leduyhung141@gmail.com>
2023-04-27 10:22:12 +0700

    modify variable

--- src/NV/main.java
+++ src/NV/main.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        byte choice = 0;
+        byte chon = 0;
         do {
             System.out.println("1. Nhập thông tin nhân viên");
             System.out.println("2. Hiển thị thông tin tất cả nhân viên");
             System.out.println("3. Tìm kiếm nhân viên theo mã nv");
             System.out.println("4. Sắp xếp nhân viên theo tên");
             System.out.println("5. Thoát");
-            choice = scanner.nextByte();
-            while (choice < 1 || choice > 5) {
+            chon = scanner.nextByte();
+            while (chon < 1 || chon > 5) {
                 System.out.println("Xin mời nhập lại");
-                choice = scanner.nextByte();
+                chon = scanner.nextByte();
             }
             QLNV qlnv = new QLNV();
-            switch (choice) {
+            switch (chon) {
                 case 1: {
                     qlnv.nhapThongTinNV();
                     break;
@@ -44,7 +44,7 @@ public class main {
                 }
             }
             System.out.println("***************************************");
-        } while (choice != 5);
+        } while (chon != 5);
 
     }
 }
